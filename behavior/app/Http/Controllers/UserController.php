0293b14c01e7550eb7049b608f621a246d0721ab
Filipe <lipecavalcante17@hotmail.com>
2022-09-27 23:51:24 -0300

    Trabalhando com consultas join e where

--- behavior/app/Http/Controllers/UserController.php
+++ behavior/app/Http/Controllers/UserController.php
@@ -42,15 +42,55 @@ class UserController extends Controller
 //            echo "<br>";
 //        }
 
-        DB::table('users')->where('id', '<', '500')->orderBy('id')->chunk(50, function ($users){
-            foreach ($users as $user){
-                echo "#{$user->id} Nome: {$user->name}";
-                echo "Status: {$user->status}";
-                echo "<br>";
-            }
+//        DB::table('users')->where('id', '<', '500')->orderBy('id')->chunk(50, function ($users){
+//            foreach ($users as $user){
+//                echo "#{$user->id} Nome: {$user->name}";
+//                echo "Status: {$user->status}";
+//                echo "<br>";
+//            }
+//
+//            echo 'Encerrou um ciclo <br>';
+//            sleep(1);
+//        });
 
-            echo 'Encerrou um ciclo <br>';
-            sleep(1);
-        });
+//        $users = DB::table('users')
+////            ->whereIn('status', array(0, 1))
+////            ->whereNotIn('status', array(0, 1))
+////            ->whereNull('')
+//            ->whereNotNull('name')
+////            ->whereColumn('created_at', '=', 'updated_at')
+////            ->whereDate('updated_at', '>', '2022-09-27')
+//            ->whereDay('updated_at', '=', '1')
+//            ->whereMonth('updated_at', '=', '1')
+//            ->whereYear('updated_at', '=', '2019')
+//            ->whereTime('updated_at', '>', '17:30:00')
+//            ->get();
+//
+//        foreach ($users as $user){
+//            echo "#{$user->id} Nome: {$user->name}";
+//            echo "Status: {$user->status}";
+//            echo "<br>";
+//        }
+
+        $users = DB::table('users')
+            ->select('users.id', 'users.name', 'users.status', 'addresses.address')
+//            ->leftJoin('addresses', 'users.id', '=', 'addresses.user')
+            ->join('addresses', function ($join){
+                $join->on('users.id', '=', 'addresses.user')
+                     ->where('addresses.status', '=', '1');
+            })
+            ->orderBy('users.id', 'ASC')
+            ->get();
+
+        echo "Todos os registros: {$users->count()}";
+        echo "<br>";
+
+        foreach ($users as $user){
+            echo "#{$user->id} Nome: {$user->name}";
+            echo "Status: {$user->status}";
+            echo "Endereço: {$user->address}";
+            echo "<br>";
+        }
+
     }
 }
