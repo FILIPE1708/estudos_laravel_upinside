14994c4ea69deebd971144ba630527f2da923a6b
Filipe <lipecavalcante17@hotmail.com>
2022-09-28 21:01:44 -0300

    Criação de sessions

--- behavior/routes/web.php
+++ behavior/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +25,41 @@ Route::get('/logs', function (){
 });
 
 Route::get('/session', function (){
-    
+//   session('', '')->all();
+
+    session(array(
+        'course' => "Laradev"
+    ));
+    session()->put('name', 'Teste');
+
+//    echo session('student', function (){
+//        session()->put('student', 'Teste');
+//        return session('student');
+//    });
+
+//    echo session()->get('name');
+
+//    session()->push('time', 'aiii');
+
+//    $student = session()->pull('student');
+//    echo $student;
+
+//    session()->forget('name');
+
+//    session()->flush();
+
+//   if(session()->has('course')){
+//       echo 'O curso selecionado foi: ' . session()->get('course');
+//   }
+//
+//   if(session()->exists('student')){
+//       echo 'Esse índice existe';
+//   }else{
+//       echo 'Esse índice não existe';
+//   }
+
+    session()->flash('message', 'O artigo foi adicionado com sucesso');
+    session()->reflash();
+
+   var_dump(session()->all());
 });
